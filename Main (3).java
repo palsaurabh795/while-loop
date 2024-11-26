8e434b87e5413d6d5f66bc739d7acb2c33f995ea
Saurabh Pal <pal.saurabhh@gmail.com>
2024-11-26 11:16:17 +0530

    Add files via upload
    java while loop

---
+++ Main (3).java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+
+public static void main(String args []) {
+	
+	Scanner sc = new Scanner(System.in);
+	Boolean learn = false;
+	while(learn) {
+		System.out.println("went to school , try to learn");
+		learn =  sc.nextBoolean();
+		
+	}
+}
+	
+}
